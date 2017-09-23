3ee20bc4d1ff99478cf1fc813048dafbbc426d3c
Cameron Higgins <cameron.higgins@me.com>
2017-09-23 01:08:13 -0400

    Fixes db connection bug for cloud platform.

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 let dbURI = 'mongodb://localhost/Loc8r';
 if (process.env.NODE_ENV === 'production') {
-  dbURI = process.env.MONGODB_URI;
+  dbURI = process.env.MLAB_URI;
 }
 mongoose.connect(dbURI);
 
