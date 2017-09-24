ee047aa01f0dffa45bc663323a7733ccc20c2164
Cameron Higgins <cameron.higgins@me.com>
2017-09-24 14:29:23 -0400

    adds some logic for api routes

---
+++ app_api/controllers/locations.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const Loc = mongoose.model('Location');
+
+const locationsCreate = function (req, res) {
+  res
+    .status(200)
+    .json({"status" : "success"});
+};
+
+const locationsListByDistance = function (req, res) {
+  res
+    .status(200)
+    .json({"status" : "success"});
+};
+
+const locationsReadOne = function (req, res) {
+  Loc
+    .findByID(req.params.locationid)
+    .exec((err, location) => {
+      res
+        .status(200)
+        .json(location);
+    });
+};
+
+const locationsUpdateOne = function (req, res) {
+  res
+    .status(200)
+    .json({"status" : "success"});
+};
+
+const locationsDeleteOne = function (req, res) {
+  res
+    .status(200)
+    .json({"status" : "success"});
+};
+
+module.exports = {
+  locationsListByDistance,
+  locationsCreate,
+  locationsReadOne,
+  locationsUpdateOne,
+  locationsDeleteOne
+};
