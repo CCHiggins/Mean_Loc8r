41ddecc92f3cb970a4a36bf884bd77ea95bf0256
Cameron Higgins <cameron.higgins@me.com>
2017-09-24 15:16:57 -0400

    adds error handling for get routes.

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -14,13 +14,34 @@ const locationsListByDistance = function (req, res) {
 };
 
 const locationsReadOne = function (req, res) {
-  Loc
-    .findById(req.params.locationid)
-    .exec((err, location) => {
-      res
-        .status(200)
-        .json(location);
-    });
+  if (req.params && req.params.locationid) {
+    Loc
+      .findById(req.params.locationid)
+      .exec((err, location) => {
+        if (!location) {
+          res
+            .status(404)
+            .json({
+              "message": "locationid not found"
+            });
+          return;
+        } else if (err) {
+          res
+            .status(404)
+            .json(err);
+          return;
+        }
+        res
+          .status(200)
+          .json(location);
+      });
+  } else {
+    res
+      .status(404)
+      .json({
+        "message": "No locationid in request"
+      });
+  }
 };
 
 const locationsUpdateOne = function (req, res) {
